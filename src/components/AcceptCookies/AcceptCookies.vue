d7ddff6010b5ba29b4303e575e0ad397850fda92
RudychikFedorV <rudychik.fedor@gmail.com>
2024-01-25 21:44:43 +0200

    startVue

---
+++ src/components/AcceptCookies/AcceptCookies.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="AcceptCookies">
+    <div class="AcceptCookies__title">
+      This website uses cookies to enhance the user experience. Read our privacy policy
+      for more info.
+    </div>
+    <router-link to="/"
+      ><div class="AcceptCookies__button btn">Accept Cookies</div></router-link
+    >
+  </div>
+</template>
+<script></script>
+<style lang="scss" scoped>
+@import "@/components/AcceptCookies/AcceptCookies.scss";
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <HeaderBlock />
+    <AcceptCookies />
+  </div>
+</template>
+
+<script>
+import HeaderBlock from "@/components/HeaderBlock/HeaderBlock.vue";
+import AcceptCookies from "@/components/AcceptCookies/AcceptCookies.vue";
+
+export default {
+  name: "HomePage",
+  data() {
+    return {
+      isChecked: false,
+    };
+  },
+  methods: {},
+  components: {
+    HeaderBlock,
+    AcceptCookies,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
